84390ab6914f8ca98f08f1042d2c38c962ac84b5
Celine Joie V. Buenaventura <buenaventuraceline277@gmail.com>
2022-01-07 08:53:26 +0800

    Step 12

--- lib/story_brain.dart
+++ lib/story_brain.dart
@@ -44,6 +44,10 @@ class StoryBrain {
   String getChoice1() {
     return _storyData[0].choice1;
   }
+
+  String getChoice2() {
+    return _storyData[0].choice2;
+  }
 }
 
 //TODO: Step 23 - Use the storyNumber property inside getStory(), getChoice1() and getChoice2() so that it gets the updated story and choices rather than always just the first (0th) one.
