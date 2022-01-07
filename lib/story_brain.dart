e36f4fe485adb0199386e96d34d4c7f91a7be9d2
Celine Joie V. Buenaventura <buenaventuraceline277@gmail.com>
2022-01-07 08:28:46 +0800

    Step 5

--- lib/story_brain.dart
+++ lib/story_brain.dart
@@ -3,7 +3,7 @@
 //TODO: Step 5 - Create a new class called StoryBrain.
 
 //TODO: Step 7 - Uncomment the lines below to include storyData as a private property in StoryBrain. Hint: You might need to change something in story.dart to make this work.
-
+class StoryBrain {}
 //List<Story> _storyData = [
 //  Story(
 //      storyTitle:
